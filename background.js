3ca3f352f86f2dae616694385d4bf4fafd910829
nickmpaz <44784159+nickmpaz@users.noreply.github.com>
2019-02-06 13:39:22 -0800

    fix sync storage issue on update

--- background.js
+++ background.js
@@ -1,8 +1,12 @@
 //on install, set pipeline as a default surfbreak
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({sURL: ["https://www.surfline.com/surf-report/pipeline/5842041f4e65fad6a7708890","https://www.surfline.com/surf-report/blacks/5842041f4e65fad6a770883b","https://www.surfline.com/surf-report/el-porto/5842041f4e65fad6a7708906"]}, function() {
-      console.log('sURL set to pipeline');
-    });
+  chrome.storage.sync.get('sURL', function(data){
+    if (typeof data.sURL === "undefined") {
+      chrome.storage.sync.set({sURL: ["https://www.surfline.com/surf-report/pipeline/5842041f4e65fad6a7708890","https://www.surfline.com/surf-report/blacks/5842041f4e65fad6a770883b","https://www.surfline.com/surf-report/el-porto/5842041f4e65fad6a7708906"]}, function() {
+        console.log('sURL set to pipeline');
+      });
+    }
+  });
 })
 
 
